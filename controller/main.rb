2da45dd80797659dfed8fe55f120338f92898ab8
Pistos <gitsomegrace.5.pistos@geoshell.com>
2009-02-01 14:16:50 -0500

    Added basic login and logout using User helper.

--- controller/main.rb
+++ controller/main.rb
@@ -1,4 +1,20 @@
 class MainController < Ramaze::Controller
-  layout '/page' => [ :index ]
+  layout '/page' => [ :index, :login, :logout ]
+  helper :user
 
+  # ----------------------------------------------
+
+  def index
+
+  end
+
+  def login
+    if request.post?
+      user_login
+    end
+  end
+
+  def logout
+    user_logout
+  end
 end
--- model/user.rb
+++ model/user.rb
@@ -1,2 +1,15 @@
+require 'digest/sha1'
+
 class User < DBI::Model( :users )
+
+  def self.authenticate( credentials )
+    return nil  if credentials.nil? || credentials.empty?
+
+    encrypted_password = Digest::SHA1.hexdigest( credentials[ 'password' ] )
+    User.one_where(
+      :username => credentials[ 'username' ],
+      :encrypted_password => encrypted_password
+    )
+  end
+
 end

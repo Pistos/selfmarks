2da91f5124fdd2c389a2cba34e9d534ec71fcd40
Pistos <gitsomegrace.5.pistos@geoshell.com>
2009-02-02 12:03:47 -0500

    Added Bookmark#tags( user ) and Bookmark#notes( user ).

--- model/bookmark.rb
+++ model/bookmark.rb
@@ -27,4 +27,36 @@ class Bookmark < DBI::Model( :bookmarks )
       self.id
     )
   end
+
+  def tags( user )
+    $dbh.s(
+      %{
+        SELECT
+          tags.name
+        FROM
+            tags
+          , users_bookmarks_tags ubt
+        WHERE
+          ubt.user_id = ?
+          AND ubt.bookmark_id = ?
+          AND tags.id = ubt.tag_id
+      },
+      user.id,
+      self.id
+    )
+  end
+
+  def notes( user )
+    $dbh.sc(
+      %{
+        SELECT notes
+        FROM users_bookmarks
+        WHERE
+          user_id = ?
+          AND bookmark_id = ?
+      },
+      user.id,
+      self.id
+    )
+  end
 end

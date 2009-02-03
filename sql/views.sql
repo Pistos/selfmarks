4c332b2c87b28acf871efb319aaea35f15025e7a
Pistos <gitsomegrace.5.pistos@geoshell.com>
2009-02-03 10:19:39 -0500

    Added user_bookmarks view.

---
+++ sql/views.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE VIEW user_bookmarks AS
+SELECT
+    b.uri,
+    ub.bookmark_id,
+    ub.user_id,
+    ub.time_created,
+    ub.title,
+    ub.notes
+FROM
+      bookmarks b
+    , users_bookmarks ub
+WHERE
+    b.id = ub.bookmark_id
+;

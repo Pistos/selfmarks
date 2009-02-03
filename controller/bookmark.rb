Pistos/selfmarks

commit c657fdb06bf4a98912754c155fa77ea8e1759c7e

Use bookmark_ensure in /uri/edit .

controller/bookmark.rb
@@ -43,8 +43,11 @@ class BookmarkController < Ramaze::Controller
     end
 
     if request.post?
-      bm.set_title( user, h( request[ 'title' ] ) )
-      bm.set_notes( user, h( request[ 'notes' ] ) )
+      user.bookmark_ensure(
+        bm,
+        h( request[ 'title' ] ),
+        h( request[ 'notes' ] )
+      )
       bm.tags_ensure( requested_tags, user )
       flash[ :success ] = "Updated #{bm.uri}."
     end

commit ffa91dfe24c2d6b6a27577decb5aaa3b133373ba

Fixed /uri/edit as broken by commit c657fdb.

controller/bookmark.rb
@@ -48,11 +48,11 @@ class BookmarkController < Ramaze::Controller
     end
 
     if request.post?
-      user.bookmark_ensure(
-        bm,
-        h( request[ 'title' ] ),
-        h( request[ 'notes' ] )
-      )
+      title = h( request[ 'title' ] )
+      notes = h( request[ 'notes' ] )
+      user.bookmark_ensure( bm, title, notes )
+      bm.set_title( user, title )
+      bm.set_notes( user, notes )
       bm.tags_ensure( requested_tags, user )
       flash[ :success ] = "Updated #{bm.uri}."
     end
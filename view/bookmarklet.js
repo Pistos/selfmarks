7edd363d4a16e372c09fe8d91a4ab1aaff40fd12
Pistos <gitsomegrace.5.pistos@geoshell.com>
2009-02-03 12:29:53 -0500

    load_js now takes an id to check for existence first, so as not to overpopulate the DOM with duplicates.

--- view/bookmarklet.js
+++ view/bookmarklet.js
@@ -1,7 +1,12 @@
-function load_js( uri ) {
+function load_js( uri, id ) {
+    var script_tag = document.getElementById( id );
+    if( script_tag ) {
+        script_tag.parentNode.removeChild( script_tag );
+    }
     var script  = document.createElement( 'SCRIPT' );
     script.type = 'text/javascript';
     script.src  = uri;
+    script.id   = id;
     document.getElementsByTagName('head')[0].appendChild( script );
 }
 

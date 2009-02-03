2b99e4948dbe70251a372aa0d99f419061925d31
Pistos <gitsomegrace.5.pistos@geoshell.com>
2009-02-03 10:20:18 -0500

    Added Tag#bookmarks_of( user ).

--- model/tag.rb
+++ model/tag.rb
@@ -2,4 +2,23 @@ class Tag < DBI::Model( :tags )
   def to_s
     name
   end
+
+  def bookmarks_of( user )
+    UserBookmark.s(
+      %{
+        SELECT
+          ub.*
+        FROM
+            user_bookmarks ub
+          , users_bookmarks_tags ubt
+        WHERE
+          ubt.tag_id = ?
+          AND ubt.user_id = ?
+          AND ub.user_id = ubt.user_id
+          AND ub.bookmark_id = ubt.bookmark_id
+      },
+      self.id,
+      user.id
+    )
+  end
 end

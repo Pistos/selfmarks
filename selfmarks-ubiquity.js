82f6db88decf36ff75174ca3323c1e81a9f6daed
Pistos <gitsomegrace.5.pistos@geoshell.com>
2009-02-03 09:11:36 -0500

    Added Ubiquity script; contributed by manveru.

---
+++ selfmarks-ubiquity.js
@@ -0,0 +1,45 @@
+CmdUtils.CreateCommand({
+  name: "selfmark",
+  homepage: "http://manveru.net",
+  author: { name: "Michael Fellinger", email: "m.fellinger@gmail.com"},
+  contributors: ["Michael Fellinger"],
+  license: "MIT",
+  description: "Tags the current site using selfmarks",
+  icon: "http://sm.purepistos.net/favicon.ico",
+  help: "Save the current url to selfmarks with the tags input by the user. Any selected text on the page will be recorded as the note.",
+
+  takes: {note: noun_arb_text},
+  modifiers: {
+    tagged: noun_arb_text,
+    titled: noun_arb_text
+  },
+
+  preview: function(pblock, note, mods){
+    var params = this.post_params(note, mods);
+
+    html =  "<p>title: " + params.title + "</p>";
+    html += "<p>tags: "  + params.tags  + "</p>";
+    html += "<p>note: "  + params.notes + "</p>";
+
+    pblock.innerHTML = html;
+  },
+
+  execute: function(note, mods){
+    var params = this.post_params(note, mods);
+    var response = jQuery.post("http://sm.purepistos.net/uri/add", params);
+  },
+
+  post_params: function(note, mods){
+    var doc =  Application.activeWindow.activeTab.document;
+    var uri = Utils.url(doc.documentURI);
+
+    var params = {
+      uri: uri.asciiSpec,
+      title: mods.titled.text || doc.title,
+      notes: note.text,
+      tags: mods.tagged.text
+    };
+
+    return params;
+  }
+});

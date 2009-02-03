5dbc2819e6a9ae72cb025b154ca12c038ad0455d
Pistos <gitsomegrace.5.pistos@geoshell.com>
2009-02-03 12:30:30 -0500

    add_window now puts our CSS on the page so the window looks good.

--- view/uri/add_window.js
+++ view/uri/add_window.js
@@ -1,4 +1,7 @@
 if( $( '#selfmarks-window' ).length == 0 ) {
+    $( 'head' ).append(
+        '<link rel="stylesheet" href="#{SelfMarks::HOST}/selfmarks.css" type="text/css" media="screen"/>'
+    );
     var div = $( '#{@window_html}' );
     div.hide();
     $( 'body' ).append( div );
